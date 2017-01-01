285c1c948a966a62fc0b3f530a10d5a633f7c17a
jaderampulla <jade.rampulla@eagleview.com>
2017-01-01 00:13:06 -0500

    Updated Default
    PHP 5 mysql to PHP 7 mysqli

--- default.php
+++ default.php
@@ -13,14 +13,14 @@ echo "<h3>$title</h3>\n";
 echo "<font style=\"color: #8B0000;\">Note: This is the default logging location</font><br />\n";
 $getlogs="SELECT timestamp,facility,level,host,msg FROM $logstable ORDER BY seq DESC LIMIT 100;";
 $getlognum="SELECT count(seq) as thecount FROM $logstable;";
-$getlogs=mysql_query($getlogs);
-$getlognum=number_format(mysql_fetch_object(mysql_query($getlognum))->thecount,0,".",",");
+$getlogs=mysqli_query($dbconn,$getlogs);
+$getlognum=number_format(mysqli_fetch_object(mysqli_query($dbconn,$getlognum))->thecount,0,".",",");
 $time=end_time($time_start);
 echo "$getlognum total syslog messages. 100 messages displayed. SQL query completed in {$time}seconds.<br /><br />\n";
 echo "<div id=\"contenttable\">\n";
 echo "<table border='1'>\n";
 echo "<tr><th>Timestamp</th><th>Facility</th><th>Level</th><th>Host</th><th>Message</th></tr>\n";
-while($row=mysql_fetch_array($getlogs)){
+while($row=mysqli_fetch_array($getlogs)){
 	echo "<tr>";
 	echo "<td class=\"timestamp\">" . $row['timestamp'] . "</td>";
 	echo "<td>" . $row['facility'] . "</td>";
